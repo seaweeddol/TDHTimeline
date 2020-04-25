10e45a2312a9d396a0242bf55bd1c6a23bd0cf21
Jessica Stone <jstone4815@gmail.com>
2020-04-24 20:08:43 -0700

    initialize components

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+// import './App.css';
+// import Board from './components/Board';
+
+const App = () => {  
+
+  return (
+    <section>
+      <header className="">
+        <h1 className=""><span className="">TDH Timeline</span></h1>
+      </header>
+    </section>
+  );
+};
+
+export default App;
