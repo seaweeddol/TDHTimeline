880cc500236fc64951ae85aea2d8cca15a7b22d6
Jessica Stone <jstone4815@gmail.com>
2020-04-24 20:39:44 -0700

    add content to App

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-// import './App.css';
+import './App.css';
 import Timeline from './components/Timeline';
 
 const App = () => {  
@@ -7,9 +7,12 @@ const App = () => {
   return (
     <section>
       <header className="">
-        <h1 className="">TDH Timeline</h1>
+        <h1>The Dear Hunter Timeline</h1>
+        <p>An estimation of when key events in the Acts occurred.</p>
+        <p>Credit for timeline information goes to Reddit user <a href="https://www.reddit.com/user/Lesime">/u/Lesime</a> via <a href="https://www.reddit.com/r/TheDearHunter/comments/5uuus9/the_dear_hunter_timeline/">this post</a></p>
+        <p>Visit <a href="http://thedearhunter.com/">The Dear Hunter</a></p>
       </header>
-      <
+      <Timeline />
     </section>
   );
 };
